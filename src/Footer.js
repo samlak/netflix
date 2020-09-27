b6f6fe5af6cc90488123ced8c4b51abe8227e73c
samlak <samlak1999@gmail.com>
2020-09-27 21:02:29 +0100

    Footer added

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+	return (
+		<div className="footer" >
+			Made with  <span role="img" aria-label="Love"> 💖 </span> by samlak &nbsp;
+			<a href="https://devsamlak.netlify.app" className="footer__link" >
+				Check out my portfolio
+			</a>
+		</div>
+	)
+};
+
+export default Footer;
